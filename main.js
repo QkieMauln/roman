776e6b028ef887a7554746a12ee8629201b2f96a
qky <qkie0915@gmail.com>
2020-01-29 13:13:38 +0700

    some commits

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
 // <!> Dont delete dis <!>
 
 //listener
-var enter = document.getElementById("passin");
-enter.addEventListener("keyup", function(event){
+document.getElementById("passin").addEventListener("keyup", function(event){
   if(event.keyCode === 13){
     event.preventDefault();
     waitverify('passin', 'loadbox', 'waittill');
