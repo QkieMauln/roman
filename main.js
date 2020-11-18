7403dd1501ff3a4db9f97edf4cf6135d145d6a25
Qky <43922147+QkieMauln@users.noreply.github.com>
2020-11-18 19:36:56 +0700

    fullscreen update

--- main.js
+++ main.js
@@ -357,3 +357,15 @@ function wkwkTrigger(){
     $("#waittill").show();
   },10000);
 }
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
